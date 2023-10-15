36ac1ccd92ccc1ac121cc3e0b9efb0f5f8689219
jagdeesh kumar <recs.cse17904@gmail.com>
2023-10-15 12:07:55 +0530

    first changes

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -220,7 +220,7 @@
 
     <div class="heroe">
 
-        <h1>Welcome to CodeIgniter , jagdeesh kumar<?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
+        <h1>Welcome to CodeIgniter , jagdeesh kumar maurya<?= CodeIgniter\CodeIgniter::CI_VERSION ?></h1>
 
         <h2>The small framework with powerful features</h2>
 
